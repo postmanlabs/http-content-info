1a75637bd157ed8af4e09a27e93d8273e26939aa
Shamasis Bhattacharya <mail@shamasis.net>
2016-07-04 19:16:56 +0530

    Added source license to index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+/**!
+ * The code was originally published by contributors to the <https://github.com/jshttp/content-disposition> repository.
+ * It has been reformatted and modified to be used in http-content-disposition module.
+ * 
+ * @attributions
+ *
+ * (The MIT License)
+ * 
+ * Copyright (c) 2014 Douglas Christopher Wilson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the 
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * license: https://github.com/jshttp/content-disposition/blob/82fd7db3aa663438166f970efb5e337fa63b4465/LICENSE
+ * source: <https://github.com/jshttp/content-disposition/blob/82fd7db3aa663438166f970efb5e337fa63b4465/index.js>
+ */
